a138ba48cab44ee81ec43a6084820232666cffb2
surisaran <surisaran9@gmail.com>
2025-04-04 12:03:39 +0530

    import changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from assistant import Assistant
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+#from langchain_chroma import Chroma
 #from langchain_openai import OpenAIEmbeddings
 import logging
 from sentence_transformers import SentenceTransformer
